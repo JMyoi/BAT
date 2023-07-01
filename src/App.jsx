b8c02975fb5df595db9bd2a4532562a4522a4152
Jay Chen <jaychenjjc@gmail.com>
2023-06-30 20:10:01 -0400

    still on iteration 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,43 @@
 import './App.css'
 import {useState} from 'react'
 
-
 function BudgetTracker(){
-    const[transactionInputs, setTransactionInputs] = useState([]);  
+//this state holds an array of objects with transaction, data, and amount keys.
+const[transactionInputs, setTransactionInputs] = useState([]);  
 
-    const sampleInput = [
-      {
-        transaction: "Grocery",
-        date: "06-23-2023",
-        amount: "400",
-        id:"1"
-      },
-      {
-        transaction: "coffee",
-        date: "07-23-2023",
-        amount:"3",
-        id:'2'
-      },
-      {
-        transaction:"soap",
-        date: "08-04-2023",
-        amount: "2",
-        id:'3'
-      }
-    ];
-
-    const SampleOutput = sampleInput.map(data=>{
-      
-      return (<tr key = {data.id} className = "inputRow">
-        <td className = "tableData">{data.transaction}</td>
-        <td className = "tableData">{data.date}</td>
-        <td className = "tableData">{data.amount}</td>
-      </tr>)
-    })
-    
-    function handleSubmit(e){
-      e.preventDefault();
-      const formInput = e.target;
-      let Transaction = formInput.transactionInput.value;
-      let Date = formInput.dateInput.value;
-      let Amount = formInput.amountInput.value;
-
-      let NewInput = {
-        transaction:Transaction,
-        date:Date,
-        Amount
-      };
+function Form(){
+  function handleSubmit(e){
+    e.preventDefault();
       setTransactionInputs(
-        [ NewInput, ...transactionInputs]
+        [{
+          transaction: e.target.transactionInput.value,
+          date: e.target.dateInput.value,
+          amount: e.target.amountInput.value
+        },
+        ...transactionInputs]
       );
 
+    } 
+    
+  return(
+    <form onSubmit = {handleSubmit} class = "Form">
+    <label htmlFor = "transactionInput"> Transaction: </label>
+      <input id = "transactionInput" type="text" required placeholder='Name of Transaction'/>
+    <label htmlFor ="dateInput" >date: </label>
+      <input id = "dateInput" type="date" required/>
+    <label htmlFor = "amountInput">amount: </label>
+      <input id ="amountInput" type="number" required placeholder = '- for spendings and + for earnings'/>
+    <input type="submit" />
+  </form>
+  )
 
-      //set the transactionsInputs state with the user input from the form 
-      //then when rerender happens due to the set state function it will remember the map function to include the new user input.
-      
-      } //to handle submitting and will render the data into the table
+}
 
-
-  
   return (
     <>
-    
-    <form onSubmit = {handleSubmit}>
-      <label htmlFor = "transactionInput"> Transaction: </label>
-        <input id = "transactionInput" type="text" required/>
-      <label htmlFor ="dateInput" >date: </label>
-        <input id = "dateInput" type="date" required/>
-      <label htmlFor = "amountInput">amount: </label>
-        <input id ="amountInput" type="number" required/>
-      <input type="submit" />
-    </form>
-
-    <div id = "TableDiv">
-      <table id = "table">
+    <Form/>
+    <div id = "ContentBody">
+      <table id = "TableContent">
         <thead>
           <tr id = "headerRow">
             <th>Transaction</th>
@@ -82,28 +46,22 @@ function BudgetTracker(){
           </tr>
         </thead>
 
-        <tbody>
-          {SampleOutput}
+        <tbody id = "dataBody">
           {transactionInputs.map(
             (Inputs)=>{
               let transaction = Inputs.transaction;
               let date = Inputs.date;
-              let amount = Inputs.amout;
+              let amount = Inputs.amount;
               return(
                 <tr className = "inputRow">
                   <td className = "tableData">{transaction} </td>
                   <td className = "tableData">{date} </td>
                   <td className = "tableData">{amount}</td>
                 </tr>
-                
-              )
-            }
-          )
-            
+                )
+              }
+            )
           }
-          
-          {/*we need a map function here that will turn the transactionInputs into an array of table row elements.  */}  
-        
         </tbody>
       </table>
 
@@ -122,4 +80,5 @@ export default function App() {
     </>
   );
 }
+
 
